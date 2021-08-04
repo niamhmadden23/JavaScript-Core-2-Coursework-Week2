5bd6db467903ff3002e6a26ebbabe3da7302669f
Niamh_Madden <Niamhmadden15@gmail.com>
2021-08-04 20:14:14 +0100

    exercise 1 complete

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,6 +1,12 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+
+    arrayOfPeople.forEach(person => {
+      content.appendChild(document.createElement("h1")).innerText = person.name;
+      content.appendChild(document.createElement("h2")).innerText = person.job;
+    }); 
 }
+
 
 let people = [
   { name: "Chris", job: "Teacher" },
