c7c7845a146827644c18626694cff1e0ed26c40c
Niamh_Madden <Niamhmadden15@gmail.com>
2021-08-04 20:32:35 +0100

    Shopping list done

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,15 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(arrayOfList) {
+  let content = document.querySelector('#content');
+  let ul = document.createElement ('ul');
+
+  arrayOfList.forEach(item =>{
+    let shoppingList = document.createElement ('li');
+    shoppingList.innerText = item; 
+    ul.appendChild(shoppingList);
+  
+});
+
+content.append(ul);
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
