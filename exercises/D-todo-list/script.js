0541b16412cd80134e14971022293bbef44cd491
Niamh_Madden <Niamhmadden15@gmail.com>
2021-08-11 18:46:24 +0100

    completed exercise d

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,26 @@
 function todoList(todos) {
   // Write your code here...
+  const contentDiv = document.getElementById("content");
+  const createUl = document.createElement("ul");
+  contentDiv.appendChild(createUl);
+  
+
+    todos.forEach((task) => {
+      const createLi = document.createElement("li");
+      createLi.innerText = task.todo;
+      createUl.appendChild(createLi);
+
+      function onClick(event) {
+        if(event.target.style.textDecoration === "line-through") {
+          event.target.style.textDecoration = "none";
+        } else {
+          event.target.style.textDecoration = "line-through";
+        }
+      }
+      createLi.addEventListener("click", onClick);
+    });
+ 
+      
 }
 
 const todos = [
