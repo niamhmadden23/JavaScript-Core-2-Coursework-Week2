7fadc3f1c918fd2b496e3e70f77c868a679dd09d
Niamh_Madden <Niamhmadden15@gmail.com>
2021-08-11 19:25:24 +0100

    exercise E done

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,23 @@
 function listOfColours(colours) {
   // Write your code here...
+  // create and add elements to contentDiv and body
+  contentDiv = document.getElementById("content");
+  createSelect = document.createElement("select");
+  createP = document.createElement("p");
+  contentDiv.appendChild(createSelect);
+  document.body.appendChild(createP);
+
+  colours.forEach((colour) => {
+    let createOption = document.createElement("option");
+    createSelect.appendChild(createOption);
+    createOption.innerText= colour;
+  });
+
+  createSelect.addEventListener('change', () => {
+    createP.style.color = createSelect.value;
+    createP.innerText = `You have selected: ${createSelect.value}`
+  });
+
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
